2cd98f9f39702e424cabc94f0b6e499eb9f9a238
Priyanshu-36 <priyanshukumar9322@gmail.com>
2026-06-30 20:54:27 +0530

    done with redux part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authservice from "./Appwrite/auth";
+import { Footer, Header } from "./Components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1 className="bg-pink-500 text-2xl font-bold">Creating Blog Website</h1>
-    </>
-  );
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    authservice
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-700">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
